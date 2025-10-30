3048f65013297d08bca7e8bbee15105efabb6afe
Ihor Zhokh <igorzhoh21@gmail.com>
2025-10-30 21:28:36 +0200

    Add first Exercise

---
+++ Homework1.py
@@ -0,0 +1,26 @@
+#Exercise 1.0
+"""Given 1:
+number1 = 20
+number2 = 30
+Expected Output:
+The result is 600"""
+
+number1 = 20
+number2 = 30
+
+result = number1 * number2
+print("The result is" , result)
+
+#Exercise 1.1
+"""Given 2:
+number1 = 40
+number2 = 30
+Expected Output:
+The result is 70"""
+number1 = 40
+number2 = 30
+
+result = number1 + number2
+print("The result is" , result)
+
+
