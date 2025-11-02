492762880c06e0989e8173a667e5d0ec6a576702
Ihor Zhokh <igorzhoh21@gmail.com>
2025-11-02 17:32:08 +0200

    add new exercise

---
+++ Python Strings.py
@@ -0,0 +1,30 @@
+#Exercise Python - Modify Strings
+
+#Upper Case
+
+# text = """When I die, bury me "
+#      "On a grave mound "
+#      "Amid the wide-wide steppe)"
+#      "In my beloved Ukraine,"
+#      "In a place from where the wide-tilled fields"
+#      "And the Dnipro and its steep banks"
+#      "an be seen and"
+#      "Its roaring rapids heard."
+#      "When it carries off"""
+
+
+# print(text.upper())
+
+# text = """When I die, bury me
+# On a grave mound
+# Amid the wide-wide steppe
+# In my beloved Ukraine,
+# In a place from where the wide-tilled fields
+# And the Dnipro and its steep banks
+# Can be seen and
+# Its roaring rapids heard.
+# When it carries off"""
+#
+# print(text.lower())
+
+
