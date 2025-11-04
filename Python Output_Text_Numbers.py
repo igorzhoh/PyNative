c278ee3a3b535060bb8fd0daa14930dab1826652
Ihor Zhokh <igorzhoh21@gmail.com>
2025-11-04 17:31:31 +0200

    add new exercise

---
+++ Python Output_Text_Numbers.py
@@ -0,0 +1,10 @@
+#Python Output / Text / Numbers
+
+print("Today is November",3,",",2025, ", closer to noon.")
+
+#More grammatical
+
+month = "November"
+day = 3
+year = 2025
+print(f"Today is {month} {day}, {year}, closer to noon.")
---
+++ Python Syntax.py
@@ -0,0 +1,15 @@
+# Python Syntax
+
+a = 18
+b = 36
+c = 42
+O = 86
+print(a + c)
+print(O * c)
+
+if O > b:
+    print("Fine")
+
+print("Just a text", end=" ")
+print("set for printing", end=" ")
+print("so you can read it")
